aa63b053f538f598fad7170b30db6cbd801a4873
rozahp <github@seekme.se>
2024-04-21 17:09:43 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# checkpasswords
-Check passwords againts api.pwnedpasswords.com/range
+# Checkpasswords
+
+I simple pythonprogram that checks passwords againts https://api.pwnedpasswords.com/range
+
+## AUTHOR 
+
+Phazor / Cascade 1733 
+
+## INSPIRATION
+
+Got inspiration and examples from other coders on Github.
+
+## LICENSE
+
+Please feel free to copy, distribute and change this program in any way you like.
+
+## INSTALLATION
+
+1. virtualenv venv
+
+2. source venv/bin/activate
+
+3. pip install -r requirements.txt
+
+Example:
+
+Example:
+
+    ./checkpasswords.py -p qwerty
+    WARN: password found: <qwerty> times: 10713794 range: B1B37 api.hash: 73A05C0ED0176787A4F1574FF0075F7521E
+
+## CONFIGURATION
+
+None.
+
+## TODO
+
+Nothing yet.
---
+++ checkpasswords.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- code: utf-8 -*-
+##
+## Filename:		checkpasswords.py
+##
+## Description:		check passwords sha1-hashes againts https://api.pwnedpasswords.com/range/
+##
+
+import logging
+import requests
+import hashlib
+import json
+import argparse
+
+##
+## Create logger with handler
+##
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter(
+            '%(asctime)s : %(levelname)s : %(message)s',
+            '%Y-%m-%d %H:%M:%S')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+##====================================
+## 
+## Parse arguments from commandline
+##
+##====================================
+def parseArguments():
+
+    parser = argparse.ArgumentParser(description='Check Passwords',
+                                    usage='checkpasswords.py [-h] [OPTIONS]',
+                                    epilog='Epilog: thanks for using my program!')
+
+    ##
+    ## Arguments
+    ##
+
+    parser.add_argument('-p','--password', action='store', type=str, default=False, 
+        help='Plain Text Password to check for [Default: %(default)s]')
+    parser.add_argument('-f','--filename', action='store', type=str, default=False, 
+        help='Filename of file with passwords  [Default: %(default)s]')    
+    parser.add_argument('-v','--verbose', action='store_true', default=False, 
+        help='Verbose mode [Default: %(default)s]')
+
+    ##
+    ## Parse arguments
+    ##
+
+    options = parser.parse_args()
+
+    ##
+    ## Error checking
+    ##
+    if options.password==False and options.filename==False:
+        logger.error("No password in argument!")
+        exit(0)
+
+    return options
+
+##===========================
+##
+## Read Passwords from file
+##
+##===========================
+def readfromfile(files):
+    passwords=[]
+    for filename in files:
+        with open(filename) as fp:
+            for p in fp.readlines():
+                p=p.strip()
+                passwords.append(p)
+    return passwords
+
+##===========================
+##
+## Hash passwords
+##
+##===========================
+def hashpasswords(passwords):
+    hashlist=[]
+
+    for p in passwords:
+        h=hashlib.sha1(p.encode('utf8')).hexdigest().upper()
+        logger.debug("hash="+str(h))
+        prefix=h[:6]
+        suffix=h[6:]
+        hashlist.append((h,p))
+    return hashlist
+
+##===========================
+##
+## Check Hashes
+##
+##===========================
+def checkhashes(hashlist):
+    
+    for h,p in hashlist:
+        pre=h[:5]
+        suf=h[5:]
+        res=requests.get('https://api.pwnedpasswords.com/range/'+str(pre))
+        if res.status_code==200:
+            logger.debug("range: "+str(pre)+" suffix: "+str(suf)+" pass: "+str(p))
+            found=res.text.find(suf)
+            if found>0:
+                for f in res.text.split():
+                    rtimes=f.split(':')[1]
+                    rhash=f.split(':')[0]
+                    if suf in f:
+                        print("WARN: password found: <"+str(p)+"> times:", rtimes, "range:", pre,"api.hash:", rhash)
+            if found<0:
+                print("PASS: password NOT found in api.pwnedpasswords.com! Congratulations! Hash:", pre+suf)
+            
+##=======================
+##
+## Main
+##
+##=======================
+if __name__ == '__main__':
+
+    options=parseArguments()    
+
+    if options.verbose:
+        logger.setLevel(logging.DEBUG)
+        for handler in logger.handlers:
+            if isinstance(handler, type(logging.StreamHandler())):
+                handler.setLevel(logging.DEBUG)
+                logger.debug('Debug logging enabled')
+
+    ##
+    ## Get Passwords from file
+    ##
+    passwords=[]
+    if options.filename:
+        passwords=readfromfile(options.filename)
+    if options.password:
+        passwords.append(options.password)
+    ##
+    ## Hash files
+    ##
+    hashes=hashpasswords(passwords)
+    ##
+    ## Check hashes
+    ##
+    if len(hashes)!=0:
+        checkhashes(hashes)
+    else:
+        print("ERR:")
+
+##
+## This file has not been truncated
+##
